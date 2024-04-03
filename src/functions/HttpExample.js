a9d6944535076a39254a658d1cb1b5b7f7689e23
ParmbeerJohal <parmbeerjohal@gmail.com>
2024-04-03 01:38:31 -0700

    Test function updates

--- src/functions/HttpExample.js
+++ src/functions/HttpExample.js
@@ -6,8 +6,8 @@ app.http('HttpExample', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+        const name = request.query.get('name') || await request.text() || 'all testers';
 
-        return { body: `Hello, ${name}!` };
+        return { body: `Hello, ${name}! This is a test!` };
     }
 });
