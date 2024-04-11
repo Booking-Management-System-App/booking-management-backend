9eb55c492eedee0b8a9baccfc8d0ed53db826d4c
ParmbeerJohal <parmbeerjohal@gmail.com>
2024-04-10 23:22:59 -0700

    Create GET booking endpoints

---
+++ src/functions/GetBookings.js
@@ -0,0 +1,35 @@
+const { app } = require('@azure/functions');
+
+const { CosmosClient } = require("@azure/cosmos");
+
+const connectionString = process.env["CosmosDbConnectionString"];
+const client = new CosmosClient(connectionString);
+const database = client.database("booking-management-system");
+const container = database.container("Bookings");
+
+/**
+ * Get all bookings.
+ * @returns {object} The response object.
+ */
+app.http('GetBookings', {
+    methods: ['GET'],
+    route: 'bookings',
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log('JavaScript HTTP trigger function to get all bookings.');
+
+        const { resources: bookings } = await container.items
+            .query("SELECT * FROM c")
+            .fetchAll();
+
+        context.res = {
+            status: 200, /* Defaults to 200 */
+            body: JSON.stringify(bookings),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        return context.res;
+    }
+});
